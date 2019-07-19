a52874b1767944efa6f878d21972e97f83a43fd3
William McFall <wmcfall2@gmail.com>
2019-07-19 13:47:36 -0400

    created frontend react structure

---
+++ mern-crud-app/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import components from separate js files
+import ViewTodo from "./components/view-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import CreateTodo from "./components/create-todo.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        
+        {/* Creates navigation bar for accessing components */}
+        <nav className="navbar navbar-expand navbar-light bg-light">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link to="/" className="navbar-brand">Home</Link>
+            </div>
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+              <Link to="/" className="nav-link">View</Link>
+              </li>
+              <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* When user navigates to / the viewtodo component is rendered */}
+        <Route path="/" exact component={ViewTodo} />
+
+        {/* When user navigates to /edit/:id the edittodo component is rendered for that id */}
+        <Route path="/edit/:id" component={EditTodo} />
+
+        {/* When user navigates to /create the createtodo component is rendered */}
+        <Route path="/create/" exact component={CreateTodo} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ mern-crud-app/src/components/view-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ViewTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p> Welcome to View Todo List Component! </p>
+      </div>
+    );
+  }
+}
